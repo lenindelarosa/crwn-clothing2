a12e3f86b991f41e22e5bb43ce5b77ad488af810
Lenin <leninmanueld@gmail.com>
2022-05-06 18:35:52 -0400

    adding cart-icon with updated count

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,18 +5,15 @@ import { CartContext } from '../../contexts/cart.context';
 
 
 const CartIcon = () => {
-    const { isCartOpen, setIsCartOpen, cartItems } = useContext(CartContext);
+    const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
 
     const toogleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 
-    const totalItems = cartItems.reduce((acc, item) => 
-        acc + item.quantity, 0
-    );
 
     return (
         <div className='cart-icon-container' onClick={toogleIsCartOpen}>
             <ShoppingIcon className='shopping-icon' />
-            <span className='item-count'>{totalItems}</span>
+            <span className='item-count'>{cartCount}</span>
         </div>
     );
 };
