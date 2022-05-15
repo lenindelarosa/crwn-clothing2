ce7d00c530cb3144c486af1b85a51381b8b19ab5
Lenin <leninmanueld@gmail.com>
2022-05-15 18:14:42 -0400

    last styled components

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,11 +1,13 @@
-import { FormInputLabel, Input, Group } from './form-input.styles.jsx'
+import { FormInputLabel, Input, Group } from './form-input.styles'
 
 const FormInput = ({ label, ...otherProps }) => {
     return (
         <Group>
             <Input {...otherProps}/>
             {label && (
-                <FormInputLabel shrink={otherProps.value.length}>{label}</FormInputLabel>
+                <FormInputLabel shrink={otherProps.value.length}>
+                    {label}
+                </FormInputLabel>
             )}
         </Group> 
     );
